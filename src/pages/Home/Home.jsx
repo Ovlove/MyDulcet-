4df1c671852752a1749229176d54e673dc0007f1
Ovlove <ovwemue@gmail.com>
2025-08-12 13:13:16 +0100

    Create Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold mb-4">Welcome to MyDulcet</h1>
+      <p>Your home for articles and fiction.</p>
+      {/* Carousels and content will be added here later */}
+    </section>
+  )
+}
