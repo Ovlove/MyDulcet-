c77570172e42935feb79e4552a390b1bddd6cf1c
Ovlove <ovwemue@gmail.com>
2025-08-12 13:26:37 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import Carousel from '../../components/Carousel.jsx'
+
+const articles = [
+  {
+    image: '/images/article1.jpg',
+    title: 'Fashion Trends 2025',
+    teaser: 'Explore the hottest fashion styles coming this year...',
+  },
+  {
+    image: '/images/article2.jpg',
+    title: 'Delicious Summer Recipes',
+    teaser: 'Try these fresh and tasty dishes for sunny days.',
+  }
+]
+
+const fiction = [
+  {
+    image: '/images/fiction1.jpg',
+    title: 'The Lost Kingdom',
+    teaser: 'An epic tale of courage and adventure...',
+  },
+  {
+    image: '/images/fiction2.jpg',
+    title: 'Midnight Whispers',
+    teaser: 'A mysterious story full of secrets.',
+  }
+]
 
 export default function Home() {
   return (
-    <section>
-      <h1 className="text-3xl font-bold mb-4">Welcome to MyDulcet</h1>
-      <p>Your home for articles and fiction.</p>
-      {/* Carousels and content will be added here later */}
-    </section>
+    <>
+      <Carousel items={articles} title="Featured Articles" />
+      <Carousel items={fiction} title="Featured Fiction" />
+    </>
   )
-}
+    }
