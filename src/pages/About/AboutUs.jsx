60255a23e8ce8ee720427ef0df6509d864f45d6c
Ovlove <ovwemue@gmail.com>
2025-08-12 13:14:33 +0100

    Create AboutUs.jsx

---
+++ src/pages/About/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <section className="prose max-w-none dark:prose-invert">
+      <h1>About MyDulcet</h1>
+      <p>
+        MyDulcet is a curated space for articles and fiction, blending creativity with authentic storytelling.
+      </p>
+      <p>
+        Created and maintained by a passionate solo creator.
+      </p>
+    </section>
+  )
+}
